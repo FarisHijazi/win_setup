30868fa60c903069b43cd8c95c8aad766b2f4b2c
FarisHijazi <theefaris@gmail.com>
2023-03-30 01:54:56 +0300

    added rclone backup sync script

---
+++ rclone_backup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+curr_date="$(date +%Y%m%d_%H%M%S)"
+
+# sync C:/Users/faris to pcloud
+rclone sync /c/Users/faris/ pcloud10tb:/backups/mirror --backup-dir pcloud10tb:/backups/incremental/"$curr_date"    --filter-from rclone-sync-filter-list.txt -P -l --max-backlog=-1 --transfers=30 --buffer-size=50M --use-mmap --fast-list --drive-chunk-size 64M --retries 3 --timeout 60s --contimeout 60s &
+# sync C:/Users/faris to NAS
+rclone sync /c/Users/faris/ /z/backups/mirror --backup-dir /z/backups/incremental/"$curr_date"                      --filter-from rclone-sync-filter-list.txt -P -l --max-backlog=-1 --transfers=30 --buffer-size=50M --use-mmap --fast-list --drive-chunk-size 64M --retries 3 --timeout 60s --contimeout 60s &
+# sync C:/Users/faris to E: internal drive
+rclone sync /c/Users/faris/ /e/backups/mirror --backup-dir /e/backups/incremental/"$curr_date"                      --filter-from rclone-sync-filter-list.txt -P -l --max-backlog=-1 --transfers=30 --buffer-size=50M --use-mmap --fast-list --drive-chunk-size 64M --retries 3 --timeout 60s --contimeout 60s &
+
+# sync D: to pcloud
+rclone sync /d/ pcloud10tb:/backups/mirror --backup-dir pcloud10tb:/backups/incremental/"$curr_date"                --filter-from rclone-sync-filter-list.txt -P -l --max-backlog=-1 --transfers=30 --buffer-size=50M --use-mmap --fast-list --drive-chunk-size 64M --retries 3 --timeout 60s --contimeout 60s &
+# sync D: to NAS
+rclone sync /d/ /z/backups/mirror --backup-dir /z/backups/incremental/"$curr_date"                                  --filter-from rclone-sync-filter-list.txt -P -l --max-backlog=-1 --transfers=30 --buffer-size=50M --use-mmap --fast-list --drive-chunk-size 64M --retries 3 --timeout 60s --contimeout 60s &
+# sync D: to E: internal drive
+rclone sync /d/ /e/backups/mirror --backup-dir /e/backups/incremental/"$curr_date"                                  --filter-from rclone-sync-filter-list.txt -P -l --max-backlog=-1 --transfers=30 --buffer-size=50M --use-mmap --fast-list --drive-chunk-size 64M --retries 3 --timeout 60s --contimeout 60s &
+
+
+# -P: Shows progress while transferring files.
+# --transfers 10: Allows up to 10 file transfers to run in parallel.
+# --checkers 10: Allows up to 10 checkers to run in parallel.
+# --fast-list: Uses a faster listing mode that reduces the number of API calls.
+# --drive-chunk-size 64M: Uploads files in 64 MB chunks (increases performance).
+# --retries 3: Retries the operation up to 3 times in case of errors.
+# --timeout 60s: Sets a timeout of 60 seconds for each operation.
+# --contimeout 60s: Sets a timeout of 60 seconds for establishing a connection.
+# --drive-use-trash=false: Skips the trash and permanently deletes files when needed.
